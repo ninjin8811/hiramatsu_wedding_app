7483423fe3008b0e2d117dbc05525f7db5b3bf58
d-nakajima <nakajima.daishi0329@gmail.com>
2025-05-17 18:05:50 +0900

    各種UIの実装

--- apps/user_web/src/app/(routes)/game/[gameId]/user/_pages/QuestionPage/QuestionPage.tsx
+++ apps/user_web/src/app/(routes)/game/[gameId]/user/_pages/QuestionPage/QuestionPage.tsx
@@ -1,10 +1,17 @@
+"use client";
 import Image from "next/image";
 import styles from "./QuestionPage.module.scss";
 import SampleQuestionImage from "@/app/_images/SampleQuestionImage.png";
 import IconTimer from "@/app/_images/IconTimer.png";
 import ItemAkakora from "@/app/_images/ItemAkakora.png";
+import UserResultCorrect from "@/app/_images/UserResultCorrect.png";
+import UserResultIncorrect from "@/app/_images/UserResultIncorrect.png";
+import ItemDetailModal from "@user/_components/ItemDetailModal/ItemDetailModal";
+import { useState } from "react";
 
 export default function QuestionPage() {
+  const [showAnswer, setShowAnswer] = useState(false);
+
   return (
     <div className={styles.questionPage}>
       <div className={styles.contentWrapper}>
@@ -13,11 +20,21 @@ export default function QuestionPage() {
           <div className={styles.content_title}>
             {"出会ってから何年経つ？\n（最大文字数23文字）"}
           </div>
-          <div className={styles.content_image}>
-            <Image src={SampleQuestionImage} alt="sample question" />
-          </div>
+          {showAnswer ? (
+            <div className={styles.content_result}>
+              <Image src={UserResultCorrect} alt="sample question" />
+            </div>
+          ) : (
+            <div className={styles.content_image}>
+              <Image src={SampleQuestionImage} alt="sample question" />
+            </div>
+          )}
           <div className={styles.content_answers}>
-            <AnswerItem label="A" text="10年" />
+            <AnswerItem
+              label="A"
+              text="10年"
+              onClick={() => setShowAnswer(true)}
+            />
             <AnswerItem label="B" text="10年" />
             <AnswerItem label="C" text="10年" />
             <AnswerItem label="D" text="10年" />
@@ -44,7 +61,7 @@ export default function QuestionPage() {
             <Image src={ItemAkakora} alt="item" />
           </div>
           <div className={styles.items_item}>
-            <Image src={ItemAkakora} alt="item" />
+            {/* <Image src={ItemAkakora} alt="item" /> */}
           </div>
         </div>
         <div className={styles.status}>
@@ -52,6 +69,13 @@ export default function QuestionPage() {
           <div className={styles.status_rate}>正解率:70%</div>
         </div>
       </div>
+      <ItemDetailModal
+        image={<Image src={ItemAkakora} alt="item" />}
+        name="アカコラ"
+        description="アカコラは、アカコラの説明です。"
+        canUse={true}
+        onClose={() => {}}
+      />
     </div>
   );
 }
@@ -59,11 +83,18 @@ export default function QuestionPage() {
 type AnswerItemProps = {
   label: string;
   text: string;
+  onClick?: () => void;
+  isDisabled?: boolean;
 };
 
 function AnswerItem(props: AnswerItemProps) {
   return (
-    <div className={styles.answerItem}>
+    <div
+      className={`${styles.answerItem} ${
+        props.isDisabled ? styles.disabled : ""
+      }`}
+      onClick={props.onClick}
+    >
       <div className={styles.answerItem_label}>{props.label}</div>
       <div className={styles.answerItem_text}>{props.text}</div>
     </div>
