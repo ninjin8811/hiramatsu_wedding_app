b926c371aef30960f1e5b9c7ecd4fbc37f723bfe
d-nakajima <nakajima.daishi0329@gmail.com>
2025-06-20 02:58:22 +0900

    順番制御

--- apps/user_web/src/app/(routes)/game/[gameId]/admin/devices/page.tsx
+++ apps/user_web/src/app/(routes)/game/[gameId]/admin/devices/page.tsx
@@ -18,13 +18,30 @@ export default function DevicesPage() {
     return () => unsub && unsub();
   }, [gameId]);
 
+  // teamA~順番にソート
+  const sortedUsers = users.sort((a, b) => {
+    const teamA = a.id.toLowerCase();
+    const teamB = b.id.toLowerCase();
+    return teamA.localeCompare(teamB);
+  });
+
   return (
     <div>
       <h1>全ユーザー画面（devices）</h1>
       <div style={{ display: "flex", flexWrap: "wrap", gap: 10 }}>
-        {users.map((user) => (
+        {sortedUsers.map((user) => (
           <div key={user.id} style={{ border: "1px solid #ccc", margin: 4 }}>
             <div style={{ textAlign: "center", fontSize: 32 }}>{user.name}</div>
+            <div
+              style={{
+                textAlign: "center",
+                fontSize: 16,
+                color: "#666",
+                marginBottom: 8,
+              }}
+            >
+              {user.id}
+            </div>
             <iframe
               src={`/game/${gameId}/user/${user.id}/`}
               width={480}
