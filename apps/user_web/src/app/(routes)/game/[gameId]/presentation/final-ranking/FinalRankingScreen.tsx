563c2008be6b3bf1c2aaa08992ac91f434d17bb8
shunkat <shunkat@icloud.com>
2025-06-22 00:57:59 +0900

    fix: 最終結果画面のアニメーションをゆっくりに

--- apps/user_web/src/app/(routes)/game/[gameId]/presentation/final-ranking/FinalRankingScreen.tsx
+++ apps/user_web/src/app/(routes)/game/[gameId]/presentation/final-ranking/FinalRankingScreen.tsx
@@ -86,7 +86,7 @@ const FinalRankingScreen: React.FC<FinalRankingScreenProps> = ({ gameId }) => {
 
   return (
     <div className={styles.container}>
-      <AnimatedBackground className={styles.background} />
+      <AnimatedBackground className={styles.background} animationDuration={100}/>
 
       {/* 既存の固定背景を削除またはコメントアウト */}
       {/* <Image
